a682916cb1621e312c0a016b42f8b9b6c0e3c40f
Gbeminiyi Oke <adunolafoyinsola@gmail.com>
2024-01-19 01:42:18 +0000

    6. Build my own calculator

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
+
     calc_args = len(sys.argv) - 1
     if calc_args != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
