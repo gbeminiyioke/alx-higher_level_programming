1ea1998528add88bc5081990b4ac2be6b866589f
gbeminiyioke <adunolafoyinsola@gmail.com>
2024-01-25 05:09:24 +0100

    0. Squared simple

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,1 +1,8 @@
-hello
+#!/usr/bin/python3
+
+def square_matrix_simple(matrix=[]):
+    new_matrix = [itm[:] for itm in matrix]
+    for i, itm in enumerate(new_matrix):
+        for j, itm2 in enumerate(new_matrix):
+            new_matrix[i][j] = itm[j] ** 2
+    return new_matrix
